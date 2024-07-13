4e485f980260480fbdb63fcb2f489015e48726d7
Vishal Kumar Chaurasia <106544308+Vishal-140@users.noreply.github.com>
2024-07-14 00:40:16 +0530

    page4

---
+++ script.js
@@ -0,0 +1,70 @@
+function loadingAnimation() {
+    var tl = gsap.timeline()
+tl.from(".line h1" , {
+    y: 150,
+    stagger: 0.25,
+    duration: 0.6,
+    delay: 0.5
+})
+tl.from("#line1-part1" ,{
+    opacity: 0,
+    onStart: function(){
+        var h5timer = document.querySelector("#line1-part1 h5")
+var grow = 0
+setInterval(function(){
+    if(grow<100){
+        h5timer.innerHTML = grow++
+        
+    } else {
+        h5timer.innerHTML = grow
+    }
+},30)
+    }
+})
+
+tl.to('.line h2', {
+    animationName: "anime",
+})
+tl.to("#loader", {
+    opacity: 0,
+    duration: 0.4,
+    // delay: 4
+})
+
+tl.from("#page1", {
+    y: 1600,
+    delay: 0.2,
+    opacity: 0,
+    duration: 0.6,
+    ease: Power4
+})
+
+tl.to("#loader", {
+    display: "none"
+})
+tl.from("#nav" , {
+    opacity: 0
+})
+tl.from("#hero1 h1, #hero2 h1, #hero3 h2, #hero4 h1" , {
+    y: 120,
+    stagger: 0.2
+})
+
+}
+function cursorAnimation() {
+    document.addEventListener("mousemove", function(dets) {
+        gsap.to("#crsr",{
+            left: dets.x,
+            top: dets.y
+        })
+    })
+
+    Shery.makeMagnet("#nav-part2 h4");
+}
+loadingAnimation()
+cursorAnimation()
+
+
+
+
+
